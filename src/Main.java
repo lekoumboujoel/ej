f873eff6d18826052a38d7347f2c7a3f33570533
Joel Lekoumbou <lekoumboujoel20@gmail.com>
2026-02-09 22:02:39 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String langage = "Java";
+        String phrase = langage + " est un langage de programmation.";
+        System.out.println(phrase);
+    }
+}
